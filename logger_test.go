f4846b13ebe73a61ea8c7dc122943bb00cb4035b
Eduardo Aguilar <dante.aguilar41@gmail.com>
2024-04-16 17:18:42 -0600

    chore: save

--- logger_test.go
+++ logger_test.go
@@ -9,10 +9,6 @@ import (
 	"testing"
 
 	"github.com/danteay/golog/adapters/slog"
-<<<<<<< HEAD
-=======
-	"github.com/danteay/golog/adapters/zerolog"
->>>>>>> e2cbf83 (fix: modify mod versions)
 	"github.com/danteay/golog/internal/contextfields"
 	"github.com/danteay/golog/levels"
 	"github.com/stretchr/testify/assert"
